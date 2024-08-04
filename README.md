956461dd0d5035745e744a21f8f7be5f30b0a4cf
Ryan Bennink <benninkrs@ornl.gov>
2024-08-04 00:24:38 -0400

    Major changes: - Implements row vectors - Renames the type hierarchy - Reworks indexing - Reorganizes source code and splits into multiple files - Expands tests - v0.3.0

--- README.md
+++ README.md
@@ -7,29 +7,34 @@ pkg> add https://github.com/benninkrs/StaticBitVectors.jl
 ```
 ## Usage
 
-This package provides the abstract type `StaticBitVector` and two concrete subtypes:
- * `SBitVector` for an immutable vector of `Bool`s 
- * `MBitVector` for a mutable vector of `Bool`s
+This package provides four concrete types: 
+ * `SBitCol` for an immutable column vector of `Bool`s 
+ * `MBitCol` for a mutable column vector of `Bool`s
+ * `SBitRow` for an immutable row vector of `Bool`s 
+ * `MBitRow` for a mutable row vector of `Bool`s
 
-`SBitVector` and `MBitVector` have nearly identical interfaces, supporting most vector operations that are non length-changing. (notably, insertion/deletion of elements is currently not supported). The main difference between the two is that `MBitVector` supports `setindex!` whereas `SBitVector` does not.
+ The two column types are subtypes of `AbstractVector{Bool}`, while the row types are aliases for adjoints of column types, and are subtypes of `AbstractMatrix{Bool}`.  These four types have nearly identical interfaces, supporting most array operations that are non length-changing. (notably, insertion/deletion of elements is currently not supported). The main difference is that the two mutable types (`MBitCol` and `MBitRow`) support `setindex!`, whereas the other two do not. Collectively, these four types are called `BitVec`s.
 
-`StaticBitVector`s can be constructed in the standard way from just about any array or iterator whose elements are convertable to `Bool`.  The functions `trues` and `falses` are extended to take `SBitVector` or `MBitVector` as the first argument, which allows one to specify the output type.
+`BitVec`s can be constructed and converted from just about any array or iterator whose elements are convertable to `Bool`.  The functions `trues` and `falses` are extended to take a `BitVec` type as the first argument, which allows one to specify the output type.
 
-Bitwise operations on `StaticBitVector`s (`~`, `|`, `&`, `xor`, `nor`, and `nand`) are implemented efficiently, leveraging whole-word CPU operations to act on 64 bits at a time. An even larger set of operations (including the bitwise operators, comparisons, `min`, and, `max`) are implemented efficiently for `map` and `map!`.  Broadcasting can also be used, though it is not quite as fast as using `map` or the inherently parallel bitwise operations.  The results of such operations are `StaticBitVectors` of the same type as the input.  In the case of mixed `SBitVector` and `MBitVector` inputs, the output is an `SBitVector` (which follows the convention of `StaticArrays`).  When one of the arguments is not a `StaticBitVector`, `map` and broadcasting fall back to standard element-by-element operations.
+`BitVecs` can be indexed by integers, Cartesian indices, and iterables. **Logical indexing is not yet supported.**
+
+Bitwise operations on `BitVec`s (`~`, `|`, `&`, `xor`, `nor`, and `nand`) are implemented efficiently, leveraging whole-word CPU operations to act on 64 bits at a time. An even larger set of operations (including the bitwise operators, comparisons, `min`, and, `max`) are implemented efficiently for `map` and `map!`.  Broadcasting on arrays of the same shape can also be used, with speed comparable to that of `map` and the inherently parallel bitwise operations.  The results of such operations are `BitVecs` of the same type as the input (but see the next section for what happens in the case of mixed input types).
 
 Efficient implementations of reducing operations, including `count`, `parity`, `dot`, and `hamming` (Hamming weight and Hamming distance) are also provided.
+
 ## Example
 ```
-julia> s = SBitVector([false, true, true, false, true])
-5-element SBitVector{1}:
+julia> s = SBitCol([false, true, true, false, true])
+5-element SBitCol{1}:
  0
  1
  1
  0
  1
 
- julia> m = SBitVector((isodd(i) for i in 0:4))
-5-element MBitVector{1}:
+ julia> m = SBitCol((isodd(i) for i in 0:4))
+5-element MBitCol{1}:
  0
  1
  0
@@ -40,13 +45,13 @@ julia> s[2]
 true
 
 julia> s[4:-1:2]
-3-element SBitVector{1}:
+3-element SBitCol{1}:
  0
  1
  1
 
  julia> s & m
- 5-element SBitVector{1}:
+ 5-element SBitCol{1}:
  0
  1
  0
@@ -54,14 +59,23 @@ julia> s[4:-1:2]
  0
 
 julia> map(<=, s, m)
- 5-element SBitVector{1}:
+ 5-element SBitCol{1}:
  1
  1
  0
  1
  0
 ```
-Vertical concatenation of `StaticBitVector`s yields another `StaticBitVector`, however, horizontal concatenation yields a (non-static) `BitMatrix`.
+Vertical concatenation of `BitCol`s yields another `BitCol`; likewise, , however, horizontal concatenation yields a (non-static) `BitMatrix`.
+
+## Corner Cases
+
+For most ways of using `BitVec`s, the expected behavior is obvious.  In a few circumstances, however, it is less clear what behavior should be expected.  Some of these ambiguous circumstances and the chosen behaviors are:
+
+* Bitwise operations involving a combination of mutable and immutable `BitVec`s return an immutable `BitVec`. (This follows the convention of `StaticArray`s.)
+* Bitwise operations involving a combination of column and row `BitVec`s return a column `BitVec`.
+* Bitwise operations require `BitVecs` to have the same shape (length and orientation). 
+* In contrast, `map` only requires `BitVec` arguments to have the same lenghth, not the same orientation. (This follows the behavior of `map` for standard vector types.)
 
 
 ## Performance
--- test/runtests.jl
+++ test/runtests.jl
@@ -4,89 +4,111 @@ using Test
 
 @info "Testing Constructors"
 # Construction from explicit Bool array
-s4 = SBitVector([false, false, true, true])
-m4 = MBitVector([false, true, false, true])
-@test typeof(s4) == SBitVector{1}
-@test typeof(m4) == MBitVector{1}
+sc4 = SBitCol([false, false, true, true])
+mc4 = MBitCol([false, true, false, true])
+sr4 = SBitRow([true, false, false, true])
+mr4 = MBitRow([true, false, true, false])
+b4 = BitVector([false, true, true, false])
+@test typeof(sc4) == SBitCol{1}
+@test typeof(mc4) == MBitCol{1}
+@test typeof(sr4) == SBitRow{1}
+@test typeof(mr4) == MBitRow{1}
+
 
 # Construction from 0,1-integer array
-@test SBitVector([0,0,1,1]) == s4
-@test MBitVector([0,1,0,1]) == m4
+@test SBitCol([0,0,1,1]) == sc4
+@test MBitCol([0,1,0,1]) == mc4
+@test SBitRow([1,0,0,1]) == sr4
+@test MBitRow([1,0,1,0]) == mr4
 
 # Construction from other BitVectors
-b4 = BitVector([true,false,false,true])
-@test typeof(SBitVector(s4)) == SBitVector{1}
-@test typeof(SBitVector(s4)) == SBitVector{1}
-@test typeof(SBitVector(b4)) == SBitVector{1}
-@test typeof(MBitVector(s4)) == MBitVector{1}
-@test typeof(MBitVector(s4)) == MBitVector{1}
-@test typeof(MBitVector(b4)) == MBitVector{1}
+@test typeof(SBitCol(sc4)) == SBitCol{1}
+@test typeof(SBitCol(mr4)) == SBitCol{1}
+@test typeof(SBitCol(b4)) == SBitCol{1}
+@test typeof(MBitCol(sc4)) == MBitCol{1}
+@test typeof(MBitCol(sc4)) == MBitCol{1}
+@test typeof(MBitCol(b4)) == MBitCol{1}
 
-m4_ = m4;
-m4_mcopy = MBitVector(m4)
-m4_scopy=  SBitVector(m4);
-@test m4_ === m4
-@test m4_mcopy !== m4
-@test m4_scopy !== m4
+mc4_ = mc4;
+mc4_mcopy = MBitCol(mc4)
+mc4_scopy=  SBitCol(mc4);
+@test mc4_ === mc4
+@test mc4_mcopy !== mc4
+@test mc4_scopy !== mc4
 
 
 # cconstruction from general iterable
-@test typeof(SBitVector(isodd(i) for i in 0:255)) == SBitVector{4}
-@test typeof(MBitVector(isodd(i) for i in 0:255)) == MBitVector{4}
+@test typeof(SBitCol(isodd(i) for i in 0:255)) == SBitCol{4}
+@test typeof(MBitCol(isodd(i) for i in 0:255)) == MBitCol{4}
 
 # construction from Bool
-@test SBitVector(true) == SBitVector([true])
-@test MBitVector(true) == MBitVector([true])
+@test SBitCol(true) == SBitCol([true])
+@test MBitCol(true) == MBitCol([true])
 
 # special vectors
-@test trues(SBitVector,6) == SBitVector([true, true, true, true, true, true])
-@test trues(MBitVector,6) == MBitVector([true, true, true, true, true, true])
-@test falses(SBitVector,5) == SBitVector([false, false, false, false, false])
-@test falses(MBitVector,5) == MBitVector([false, false, false, false, false])
+@test trues(SBitCol,6) == SBitCol([true, true, true, true, true, true])
+@test trues(MBitCol,6) == MBitCol([true, true, true, true, true, true])
+@test falses(SBitCol,5) == SBitCol([false, false, false, false, false])
+@test falses(MBitCol,5) == MBitCol([false, false, false, false, false])
+@test trues(SBitRow,6) == SBitRow([true, true, true, true, true, true])
+@test trues(MBitRow,6) == MBitRow([true, true, true, true, true, true])
+@test falses(SBitRow,5) == SBitRow([false, false, false, false, false])
+@test falses(MBitRow,5) == MBitRow([false, false, false, false, false])
 
 
 @info "Testing length"
-@test length(SBitVector([0,1,1,0,1,0,0,1,0])) == 9
+@test length(SBitCol([0,1,1,0,1,0,0,1,0])) == 9
 
 
 @info "Testing indexing"
 # getting
-@test s4[2] == false
-@test s4[3] == true
-@test m4[1] == false
-@test m4[4] == true
-@test s4[[2,3,1]] == SBitVector([false, true, false])
-@test m4[4:-1:2] == MBitVector([true,false, true])
+@test sc4[2] == false
+@test sc4[3] == true
+@test mc4[1] == false
+@test mc4[4] == true
+@test sc4[[2,3,1]] == SBitCol([false, true, false])
+@test mc4[4:-1:2] == MBitCol([true,false, true])
 
 # setting
-m4_[3] = true;
-@test m4_ == MBitVector([false, true, true, true])
-@test m4 == MBitVector([false, true, true, true])
-@test m4_mcopy == MBitVector([false, true, false, true])
-@test m4_scopy == SBitVector([false, true, false, true])
-m4[3] = false;
-@test m4 == MBitVector([false, true, false, true])
-@test m4_ == MBitVector([false, true, false, true])
+mc4_[3] = true;
+@test mc4_ == MBitCol([false, true, true, true])
+@test mc4 == MBitCol([false, true, true, true])
+@test mc4_mcopy == MBitCol([false, true, false, true])
+@test mc4_scopy == SBitCol([false, true, false, true])
+mc4[3] = false;
+@test mc4 == MBitCol([false, true, false, true])
+@test mc4_ == MBitCol([false, true, false, true])
 
 
 @info "Tesing iteration"
-@test s4 == SBitVector((bit for bit in s4))
+@test sc4 == SBitCol((bit for bit in sc4))
 
 
 @info "Tesing vcat"
-@test [s4; m4] == SBitVector([0, 0, 1, 1, 0, 1, 0, 1])
+@test [sc4; mc4] == SBitCol([0, 0, 1, 1, 0, 1, 0, 1])
+@test [sr4 mr4] == SBitRow([1, 0, 0, 1, 1, 0, 1, 0])
 
 
 @info "Tesing widening arithmetic"
-@test -s4 == [0, 0, -1, -1]
-@test 1.5*m4 == [0, 1.5, 0, 1.5]
-@test s4/2 == [0, 0, 0.5, 0.5]
+@test -sc4 == [0, 0, -1, -1]
+@test 1.5*mc4 == [0, 1.5, 0, 1.5]
+@test sc4/2 == [0, 0, 0.5, 0.5]
 
 
 @info "Testing hamming and parity"
-v = SBitVector([1,0,0,1,1,0,1,0,0,1,0,1,1,1,0,1])
+v = SBitCol([1,0,0,1,1,0,1,0,0,1,0,1,1,1,0,1])
 @test count(v) == 9
 @test sum(v) == 9
 @test parity(v) == true
 @test hamming(v) == 9
-@test hamming(v, MBitVector([1,0,1,1,0,0,1,0,1,1,0,1,1,0,1,0])) == 6
+@test hamming(v, MBitCol([1,0,1,1,0,0,1,0,1,1,0,1,1,0,1,0])) == 6
+
+@info "Testing bitmapped operations"
+@test ~sc4 == SBitCol([1,1,0,0])
+@test ~mr4 == MBitRow([0,1,0,1])
+@test sc4 & mc4 == SBitCol([0,0,0,1])
+@test sc4 | mc4 == SBitCol([0,1,1,1])
+@test sc4 ⊻ mc4 == SBitCol([0,1,1,0])
+@test sr4 & mr4 == SBitRow([1,0,0,0])
+@test sr4 | mr4 == SBitRow([1,0,1,1])
+@test sr4 ⊻ mr4 == SBitRow([0,0,1,1])
